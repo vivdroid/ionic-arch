dc91146a3414971087cd2c071bcfdbc97288fd78
Vivek Bhatt <vivek.bhatt0601@gmail.com>
2019-05-15 23:48:25 +0530

    added design configuration

--- IonicArch/src/app/app-routing.module.ts
+++ IonicArch/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'auth', loadChildren: './pages/auth/auth.module#AuthModule' },
   
 ];
 
