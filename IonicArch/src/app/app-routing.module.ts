67b53f7fd34d11c518671a33c8f9f38f92f2d5c3
Vivek Bhatt <vivek.bhatt0601@gmail.com>
2019-05-28 00:29:58 +0530

    fix route path

--- IonicArch/src/app/app-routing.module.ts
+++ IonicArch/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'auth', loadChildren: './pages/auth/auth.module#AuthModule' },
   
 ];
 
